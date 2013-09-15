6442d56bda5bf797f404362026197534fe56bac6
Zivorad Baralic <obaralicltd@gmail.com>
2013-09-16 00:22:16 +0200

    Added main menu drawables. Added articles drawables.
    Worked from 00:10 to 00:20

--- Ort1/src/com/bbisercic/ort1/utilities/ImageResourceResolver.java
+++ Ort1/src/com/bbisercic/ort1/utilities/ImageResourceResolver.java
@@ -8,13 +8,13 @@ public class ImageResourceResolver {
     public static int getMainMenuItemImage(int position) {
         switch (position) {
         case 0:
-            return R.drawable.ic_launcher;
+            return R.drawable.lectures_icon;
         case 1:
-            return R.drawable.ic_launcher;
+            return R.drawable.exercices_icon;
         case 2:
-            return R.drawable.ic_launcher;
+            return R.drawable.notes_icon;
         case 3:
-            return R.drawable.ic_launcher;
+            return R.drawable.quiz_icon;
         default:
             return R.drawable.ic_launcher;
         }
